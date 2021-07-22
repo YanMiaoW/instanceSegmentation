292dad5758c1fbc3e417289c60dcf77f36867dbc
YanMiaoW <worldneedevolution@gmail.com>
2021-07-22 19:53:45 +0800

    add

--- infer.py
+++ infer.py
@@ -1,13 +1,14 @@
 import torch
 import torch.nn as nn
+import torchvision
 import numpy as np
 import cv2 as cv
 import os
 from ymlib.dataset_visual import crop_pad
+from instanceSegmentation.model.segment import Segment
 
 
 def get_instance_model(checkpoint_path='/Users/yanmiao/yanmiao/checkpoint/not_exist') -> nn.Module:
-    from instanceSegmentation.model.segment import Segment
     instance_model = Segment(3)
     if os.path.exists(checkpoint_path):
         print(f'loading model from {checkpoint_path}')
@@ -19,7 +20,7 @@ def get_instance_model(checkpoint_path='/Users/yanmiao/yanmiao/checkpoint/not_ex
     return instance_model
 
 
-def infer_instance(model, image, mask, rect=None, pad=0, bolder=0):
+def infer_instance(model: Segment, image: np.ndarray, segment_mask: np.ndarray, rect: list = None, pad=0, bolder=0) -> np.ndarray:
     h, w = image.shape[:2]
     if rect is None:
         rect = [0, 0, w, h]
@@ -27,9 +28,9 @@ def infer_instance(model, image, mask, rect=None, pad=0, bolder=0):
 
     image = crop_pad(image, xyxy=rect, bias_xyxy=[-pad, -pad, pad, pad])
 
-    if mask is not None:
-        mask = crop_pad(mask, xyxy=rect, bias_xyxy=[-pad, -pad, pad, pad])
-        image = np.bitwise_and(image, mask[:, :, np.newaxis])
+    if segment_mask is not None:
+        segment_mask = crop_pad(segment_mask, xyxy=rect, bias_xyxy=[-pad, -pad, pad, pad])
+        image = np.bitwise_and(image, segment_mask[:, :, np.newaxis])
 
     if min(*image.shape[:2]) < 50:
         return [], None, None
@@ -37,5 +38,19 @@ def infer_instance(model, image, mask, rect=None, pad=0, bolder=0):
     if bolder != 0:
         image = cv.copyMakeBorder(image, bolder, bolder, bolder, bolder, cv.BORDER_CONSTANT, value=0)
 
+    # 添加预测
+    image = 
+    input_ = torch.cat([x, heatmaps], dim=1)
+        out = self.forward(inp)
+        return torch.sigmoid(out)
 
-    return mask
+
+    instance_mask = model.test(image, segment_mask)
+
+    if bolder != 0:
+        instance_mask = crop_pad(instance_mask, bias_xyxy=[bolder, bolder, -bolder, -bolder])
+
+    x1, y1, x2, y2 = rect
+    instance_mask = crop_pad(instance_mask, bias_xyxy=[-x1 + pad, -y1 + pad, w - x2 - pad, h - y2 - pad])
+
+    return instance_mask
